705d5e7b8f5b4e975518e4fa39435b30ab185df9
Devon Taft <devon.taft@NYCMAC023.local>
2019-06-23 18:24:47 +0200

    changes

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -6,6 +6,7 @@ import Layout from '../components/Layout'
 import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
 import ImageGallery from 'react-image-gallery';
+import ReactPlayer from 'react-player'
 
 import who from '../../static/img/kr-web-images/img1.jpg'
 
@@ -49,16 +50,40 @@ export const IndexPageTemplate = ({
                 <div className="content has-text-centered">
                     <h1 className="section-heading">ABOUT ME</h1>
                   <div className="tile">
-                  <p>I started Rose Productions so that I could put all the work I've done under one name, regarding script writing and film directing. I'm currently getting my degree in Film and Television at City Varsity, and I am in my last year. I completed one year of Film and Television at UCT, then transferred to City Varsity as I wanted to gain a more practical learning experience.  My greatest passion is directing, and I've always liked to write my own scripts because then I can envision the film as I am writing, making the directing process a lot smoother. I have had 7 years of training in the dramatic arts and have received my gold medal for grade 8 drama from the London Academy of Music and Dramatic Arts (LAMDA). These years of acting experience have helped me immensely in my role as a director. I participated in the 48hour Film Festival in 2017 as the director for our team; and our film got nominated for 'Best Supporting Actress' and 'Best Original Soundtrack' (part of which I composed). 
-I am eager to work on more music videos in the near future as I find they allow for great creative freedom. I will always continue to pursue my love for film by writing and directing short films and hopefully, one day, a feature-length film.  
-</p>
+                  <p className="section-content">I started Rose Productions so that I could put all the work I've done under one name, regarding script writing and film directing. I'm currently getting my degree in Film and Television at City Varsity, and I am in my last year. 
+                      I completed one year of Film and Television at UCT, then transferred to City Varsity as I wanted to gain a more practical learning experience.  My greatest passion is directing, and I've always liked to write my own scripts because then I 
+                      can envision the film as I am writing, making the directing process a lot smoother. I have had 7 years of training in the dramatic arts and have received my gold medal for grade 8 drama from the London Academy of Music and Dramatic Arts (LAMDA). 
+                     </p>
                   </div>
+                <button>Read More</button>
+                </div>
+                <div className="content has-text-centered ">
+                    <h1 className="section-heading" style={{marginBottom: '2em !important'}}>FEATURED FILMS</h1>
+                    <div style={{backgroundColor: '#B8B8B8'}} className="tile">
+                      <div className="columns">
+                        <div className="column is-6" style={{width: '100%'}}>description</div>
+                        <ReactPlayer 
+                          className="column is-6" 
+                          url='https://www.youtube.com/watch?v=C3SFN-knjMY&t=2s'  
+                          width='100%'
+                          config={{
+                            youtube: {
+                              playerVars: { showinfo: 1 }
+                            }}}/>
+                      </div>
+                  </div>
+                  <button>Read More</button>
                 </div>
                 <div className="content has-text-centered">
-                    <h1 className="section-heading">FEATURED FILMS</h1>
+                    <h1 className="section-heading">CURRENT PROJECT</h1>
                   <div className="tile">
-
+                  <p className="section-content">
+                    I am currently in pre-production for my next film: Permanent. It is a 30-minute film and is our crew's graduate project of the year. 
+                    Permanent is a film that creates awareness of sexual harassment. It aims to end victim-shaming and victim-blaming. It strives to give 
+                    a voice to women everywhere who have been too afraid to turn the volume up, and let the world hear what they have to say...
+                  </p>
                   </div>
+                  <button>Read More</button>
                 </div>
               </div>
             </div>
